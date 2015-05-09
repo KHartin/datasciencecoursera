6a48e5daa2f1d5198e83bac59785be44de21d9c5
K <krishartin@gmail.com>
2015-05-08 20:55:09 -0700

    Create Coursera.R

---
+++ Coursera.R
@@ -0,0 +1,23 @@
+#Set Working Directory
+setwd("C:/Users/KGH/OneDrive/Coursera/R")
+
+#Write a simple function
+myfunction <- function(){
+  x <- rnorm(100)
+  mean(x)
+}
+
+#Print contents of working directory
+dir()
+
+#List contents of WD
+ls()
+
+#Write another simple function
+second <- function(x) {
+  x + rnorm(length(x))
+}
+
+#Load/read .R code from a file or connetion 
+#source("Coursera_R.R") #Really for R not RStudio
+
